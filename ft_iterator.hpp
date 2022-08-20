9a32c218839701084c9ac2f61e200b69166799cc
kmilchev <kmilchev@student.42wolfsburg.de>
2022-08-20 13:48:26 +0200

    overloaded the main operators and added tests for them

--- ft_iterator.hpp
+++ ft_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: kmilchev <kmilchev@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 17:45:10 by kmilchev          #+#    #+#             */
-/*   Updated: 2022/08/19 22:55:01 by kmilchev         ###   ########.fr       */
+/*   Updated: 2022/08/20 13:46:36 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define ITERATOR
 
 #include "ft_iterator_traits.hpp"
+	#include <iostream>
 
 namespace ft
 {
@@ -29,19 +30,24 @@ namespace ft
 	
 	private:
 		pointer _i;
-	
 	public:
 		random_access_iterator(void) : _i(nullptr){};
-		explicit random_access_iterator(pointer el) : _i(el){}; //not sure if need to be explicit
-		// random_access_iterator(const random_access_iterator& copy) : _i(copy._i) {};
+		explicit random_access_iterator(pointer el) : _i(el){};
+		random_access_iterator(const random_access_iterator& copy) : _i(copy._i) {};
 		// virtual ~random_access_iterator(void) {};
-		// random_access_iterator & operator=(const random_access_iterator other) {_i = other._i; return(*this);};
+		random_access_iterator & operator=(const random_access_iterator other) {_i = other._i; return(*this);};
 		pointer		base() const {return (_i);};
 		reference	operator*(void) const {return (*_i);};
-		pointer		operator->(void) const ;
+		pointer		operator->(void) const {return(&(operator*()));};
 		random_access_iterator operator++(int) {random_access_iterator temp(*this); _i++; return(temp);}; //post-increment;
-		random_access_iterator operator++() {_i++; return(*this);}; //post-increment;
-		reference         operator[](difference_type n) const {return *(this->_i + n);};
+		random_access_iterator &operator++() {_i++; return(*this);}; //post-increment;
+		random_access_iterator operator--(int) {random_access_iterator temp(*this); _i--; return(temp);}; //post-increment;
+		random_access_iterator &operator--() {_i--; return(*this);}; //post-increment;
+		random_access_iterator operator+(difference_type n) const {return random_access_iterator(_i + n);}
+		random_access_iterator &operator+=(difference_type n) { _i += n; return (*this);}
+		random_access_iterator operator-(difference_type n) const {return random_access_iterator(_i - n);}
+		random_access_iterator &operator-=(difference_type n) { _i -= n; return (*this);}
+		reference         operator[](difference_type n) const {return *(_i + n);};
 	
 	};
 }
--- test_iterator_class.cpp
+++ test_iterator_class.cpp
@@ -6,7 +6,7 @@
 /*   By: kmilchev <kmilchev@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 21:08:48 by kmilchev          #+#    #+#             */
-/*   Updated: 2022/08/19 22:55:45 by kmilchev         ###   ########.fr       */
+/*   Updated: 2022/08/20 13:47:42 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,11 @@
 
 int main(void)
 {
-	ft::vector<int> nums(3, 44);
+	ft::vector<int> nums(10, 44);
 	ft::vector<int> nums2(3, 33);
 
 	std::cout << "Initialising an iterator " <<std::endl;
+	ft::vector<int>::iterator it0;
 	ft::vector<int>::iterator it(nums.begin());
 	ft::vector<int>::iterator it2 = nums2.begin();
 	ft::vector<int>::iterator *it3 = &it2;
@@ -30,23 +31,53 @@ int main(void)
 	std::cout << COLOUR_BLUE << "Testing the equals operator" << COLOUR_DEFAULT << std::endl;
 	it = it2;
 	std::cout << "Should be 33: " << COLOUR_GREEN <<  *(it.base()) << COLOUR_DEFAULT << std::endl;
+	
 	std::cout << COLOUR_BLUE << "Testing method base()" << COLOUR_DEFAULT << std::endl;
 	std::cout << "Should be 44: " << COLOUR_GREEN << *(it.base()) << COLOUR_DEFAULT << std::endl;
+	
 	std::cout << COLOUR_BLUE << "Testing operator*" << COLOUR_DEFAULT << std::endl;
 	std::cout << "Should be 44: " << COLOUR_GREEN << *it << COLOUR_DEFAULT << std::endl;
+	
 	std::cout << COLOUR_BLUE << "Testing the operator->" << COLOUR_DEFAULT << std::endl;
-	std::cout << "Should be an address: " <<  COLOUR_GREEN << it3->base() << COLOUR_DEFAULT << std::endl;
+	std::cout << "Should be an address: " <<  COLOUR_GREEN << it.operator->() << COLOUR_DEFAULT << std::endl;
+	
 	std::cout << COLOUR_BLUE << "Testing the operator[]" << COLOUR_DEFAULT << std::endl;
 	it[0] = 7;
-	std::cout << "Should be 7: " <<  COLOUR_GREEN << *it << COLOUR_DEFAULT << std::endl;
-	std::cout << COLOUR_BLUE << "Testing the operator++(int) (post-increment)" << COLOUR_DEFAULT << std::endl;
-	it[1] = 8; it++;
-	std::cout << "Should be 8: " <<  COLOUR_GREEN << *it<< COLOUR_DEFAULT << std::endl;
-	std::cout << COLOUR_BLUE << "Testing the operator++ (pre_increment)" << COLOUR_DEFAULT << std::endl;
+	it[1] = 8;
 	it[2] = 9;
-	// ++it;
-	std::cout << "Should be 8: " <<  COLOUR_GREEN << it[0] << COLOUR_DEFAULT << std::endl;
+	std::cout << "Should be 7: " <<  COLOUR_GREEN << *it << COLOUR_DEFAULT << std::endl;
+	std::cout << "Should be 8: " <<  COLOUR_GREEN << it[1] << COLOUR_DEFAULT << std::endl;
+	std::cout << "Should be 9: " <<  COLOUR_GREEN << it[2] << COLOUR_DEFAULT << std::endl;
+
+	std::cout << COLOUR_BLUE << "Testing the operator++(int) (post-increment)" << COLOUR_DEFAULT << std::endl;
+	std::cout << "Should be 7: " <<  COLOUR_GREEN << *(it++) << COLOUR_DEFAULT << std::endl;
+	std::cout << "Should be 8: " <<  COLOUR_GREEN << *it<< COLOUR_DEFAULT << std::endl;
 	
+	std::cout << COLOUR_BLUE << "Testing the operator++ (pre-increment)" << COLOUR_DEFAULT << std::endl;
+	std::cout << "Should be 9: " <<  COLOUR_GREEN << *(++it) << COLOUR_DEFAULT << std::endl;
+	
+	std::cout << COLOUR_BLUE << "Testing the operator--(int) (post-decrement)" << COLOUR_DEFAULT << std::endl;
+	std::cout << "Should be 9: " <<  COLOUR_GREEN << *(it--) << COLOUR_DEFAULT << std::endl;
+	std::cout << "Should be 8: " <<  COLOUR_GREEN << *it<< COLOUR_DEFAULT << std::endl;
+	
+	std::cout << COLOUR_BLUE << "Testing the operator-- (pre-decrement)" << COLOUR_DEFAULT << std::endl;
+	std::cout << "Should be 7: " <<  COLOUR_GREEN << *(--it) << COLOUR_DEFAULT << std::endl;
+	
+	std::cout << COLOUR_BLUE << "Testing the operator+" << COLOUR_DEFAULT << std::endl;
+	std::cout << "Should be 9: " <<  COLOUR_GREEN << *(it + 2) << COLOUR_DEFAULT << std::endl;
+	
+	std::cout << COLOUR_BLUE << "Testing the operator+=" << COLOUR_DEFAULT << std::endl;
+	it += 2;
+	std::cout << "Should be 9: " <<  COLOUR_GREEN << *it << COLOUR_DEFAULT << std::endl;
+	
+	std::cout << COLOUR_BLUE << "Testing the operator-" << COLOUR_DEFAULT << std::endl;
+	std::cout << "Should be 7: " <<  COLOUR_GREEN << *(it - 2) << COLOUR_DEFAULT << std::endl;
+	
+	std::cout << COLOUR_BLUE << "Testing the operator-=" << COLOUR_DEFAULT << std::endl;
+	it -= 2;
+	std::cout << "Should be 7: " <<  COLOUR_GREEN << *it << COLOUR_DEFAULT << std::endl;
+
+
 	
 	// std::vector<int>::iterator og;
 	// std::vector<int>::iterator *gg = &og;
