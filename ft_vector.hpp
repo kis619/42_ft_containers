ce90310d4967ab4c0b4d181fadb1fa25b7669ca6
kmilchev <kmilchev@student.42wolfsburg.de>
2022-08-21 21:07:10 +0200

    added the needed defines

--- ft_vector.hpp
+++ ft_vector.hpp
@@ -6,7 +6,7 @@
 /*   By: kmilchev <kmilchev@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 19:08:20 by kmilchev          #+#    #+#             */
-/*   Updated: 2022/08/21 19:27:14 by kmilchev         ###   ########.fr       */
+/*   Updated: 2022/08/21 21:06:45 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,16 @@ class vector
 public:
 	typedef T														value_type;
 	typedef Allocator												allocator_type;
+	typedef typename allocator_type::reference						reference;
+	typedef typename allocator_type::const_reference				const_reference;
 	typedef typename allocator_type::pointer						pointer;
 	typedef typename allocator_type::const_pointer					const_pointer;
-	typedef typename allocator_type::size_type						size_type;
 	typedef	typename ft::random_access_iterator<value_type>			iterator;
 	typedef	typename ft::const_random_access_iterator<value_type>	const_iterator;
 	typedef typename ft::reverse_iterator<iterator>					reverse_iterator;
 	typedef typename ft::reverse_iterator<const_iterator>			const_reverse_iterator;
+	typedef typename iterator_traits<iterator>::difference_type		difference_type;
+	typedef typename allocator_type::size_type						size_type;
 	
 //////////////////////////////////CONSTRUCTORS//////////////////
 //default constructor 
