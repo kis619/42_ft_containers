7f60aa0da1d7e14ddf0806e049c6dfe8a9a9efa2
kmilchev <kmilchev@student.42wolfsburg.de>
2022-08-27 18:19:19 +0200

    assert tests fot vector construction

--- tests/test_vector_construction.cpp
+++ tests/test_vector_construction.cpp
@@ -6,7 +6,7 @@
 /*   By: kmilchev <kmilchev@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 13:20:18 by kmilchev          #+#    #+#             */
-/*   Updated: 2022/08/26 20:56:57 by kmilchev         ###   ########.fr       */
+/*   Updated: 2022/08/27 18:19:00 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,39 +18,45 @@
 int main(void)
 {
 	std::cout << COLOUR_BLUE << "Testing the default constructor " << COLOUR_DEFAULT << std::endl;
-	// ft::vector<int> test;
-	// std::cout << "Capcity should be 0: " << COLOUR_GREEN << test.capacity() << COLOUR_DEFAULT << std::endl;
-
+	ft::vector<int> test;
+	assert(0 == test.capacity());
+	assert(0 == test.size());
 	
-	// std::cout << COLOUR_BLUE << "Testing the fill constructor " << COLOUR_DEFAULT << std::endl;
-	// ft::vector<int> test1(4, 69);
-	// std::cout << "Size should be 4: " << COLOUR_GREEN << test1.capacity() << COLOUR_DEFAULT << std::endl;
-	// for (int i = 0; i < test1.size(); i++)
-	// 	std::cout << "Element " << i << " should be 69: " << COLOUR_GREEN << *(test1.begin()) << COLOUR_DEFAULT << std::endl;
-		
+	std::cout << COLOUR_BLUE << "Testing the fill constructor " << COLOUR_DEFAULT << std::endl;
+	ft::vector<int> test1(4, 69);
+	assert(4 == test1.capacity());
+	assert(4 == test1.size());
+	for (int i = 0; i < test1.size(); i++)
+		assert(69 == *(test1.begin()));
 	
-	// std::cout << COLOUR_BLUE << "Testing the range constructor " << COLOUR_DEFAULT << std::endl;
-	// ft::vector<int>::iterator it;
-	// ft::vector<int>::iterator it2;
-	ft::vector<int> numbers(6, 33);
-	// std::vector<int> whatever(6, 33);
-	// std::cout << numbers.begin().base();
-	// // numbers[0] = 23;
-	// // numbers[1] = 14;
-	// // numbers[2] = 51;
-	// // it = numbers.begin();
-	// // it2 = numbers.end();
-	// ft::vector<int> numbers2(it, it2);
-	// std::cout << "\nCapacity should be 3: " << COLOUR_GREEN << numbers2.capacity() << COLOUR_DEFAULT << std::endl;
-	// std::cout << "Element 0 should be 23: " << COLOUR_GREEN << *(numbers2.begin()) << COLOUR_DEFAULT << std::endl;
-	// std::cout << "Element 0 should be 14: " << COLOUR_GREEN << *(numbers2.begin() + 1) << COLOUR_DEFAULT << std::endl;
-	// std::cout << "Element 0 should be 51: " << COLOUR_GREEN << *(numbers2.begin() + 2) << COLOUR_DEFAULT << std::endl;
-	// std::cout << numbers.capacity();
-	ft::vector<int> new_vec(numbers);
-	// std::vector<int> new_vec_og(whatever);
-	for (int i = 0; i < new_vec.size(); i++)
 	{
-		std::cout << new_vec[i] << " ";
+		std::cout << COLOUR_BLUE << "Testing the range constructor " << COLOUR_DEFAULT << std::endl;
+		ft::vector<int>::iterator it;
+		ft::vector<int>::iterator it2;
+		ft::vector<int> numbers(6, 33);
+		numbers[0] = 23;
+		numbers[1] = 14;
+		numbers[2] = 51;
+		it = numbers.begin();
+		it2 = numbers.end();
+		ft::vector<int> numbers2(it, it2);
+		assert(6 == numbers2.capacity());
+		assert(23 == *(numbers2.begin()));
+		assert(14 == *(numbers2.begin() + 1));
+		assert(51 == *(numbers2.begin() + 2));
+		assert(33 == *(numbers2.begin() + 3));
+		assert(33 == *(numbers2.begin() + 4));
+		assert(33 == *(numbers2.begin() + 5));
 	}
+
+	{
+		ft::vector<int> numbers(6, 33);
+		ft::vector<int> new_vec(numbers);
+		std::vector<int> whatever(6, 33);
+		std::vector<int> new_vec_og(whatever);
+		for (int i = 0; i < new_vec.size(); i++)
+			assert((new_vec[i] == whatever[i]) == (numbers[i] == new_vec_og[i]));
+	}
+	
 	return (0);
 }
