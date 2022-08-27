5e09db4514a3975753df88268880853974a6660a
kmilchev <kmilchev@student.42wolfsburg.de>
2022-08-27 17:46:56 +0200

    fixed the reverse_iterator tests. did them with assert

--- tests/test_reverse_iterator.cpp
+++ tests/test_reverse_iterator.cpp
@@ -6,7 +6,7 @@
 /*   By: kmilchev <kmilchev@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 17:57:04 by kmilchev          #+#    #+#             */
-/*   Updated: 2022/08/24 17:23:51 by kmilchev         ###   ########.fr       */
+/*   Updated: 2022/08/27 17:46:37 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,47 +18,21 @@
 
 int main(void)
 {
-	std::vector<int> numbers;
-	
+	ft::vector<int> nums;
 	for(int i = 0; i < 10; i++)
-		numbers.push_back(i);
+		nums.push_back(i);
+	ft::vector<int>::reverse_iterator rewe(nums.rbegin());
+	ft::vector<int>::reverse_iterator rewe_1(nums.rend() - 1);
+	ft::vector<int>::reverse_iterator r;
+	
+	std::cout << COLOUR_BLUE << "Testing the reverse_iterator method .rbegin()" << COLOUR_RED << std::endl;
+	assert(9 == *rewe);
+	
+	std::cout << COLOUR_BLUE << "Testing the reverse_iterator method .rend()" << COLOUR_RED << std::endl;
+	assert(0 == *rewe_1);
+	r = nums.rbegin();
 
-	std::vector<int>::iterator it(numbers.begin());
-	std::cout << *it <<std::endl;
-	std::vector<int>::reverse_iterator rev_it(numbers.rbegin());
-	
-	std::cout << *rev_it <<std::endl;
-	// std::cout << *(numbers.begin()) <<std::endl;
-	// std::cout << *(numbers.rbegin()) <<std::endl;
-
-	for (; (*rev_it != *(numbers.begin())); rev_it++)
-		std::cout << *rev_it <<std::endl;
-	
-	// ft::vector<int> nums(7, 77);
-	// ft::vector<int>::reverse_iterator rewe(nums.begin() + 1);
-	// std::cout << *(rewe) << std::endl;
-	
-	// rewe[0] = 0;
-	// rewe[1] = 1;
-	// rewe[2] = 2;
-	// rewe[0] = 0;
-	// rewe[1] = 1;
-	// rewe[2] = 2;
-	
-	// std::cout << *(rewe);
-	// std::vector<int>::reverse_iterator sth;
-	// std::vector<int>::iterator sth1;
-	// // std::cout << *sth;
-	// std::cout << *sth1;
-	// std::cout << numbers[0] <<std::endl;
-	// std::cout << numbers[1] <<std::endl;
-	// std::cout << numbers[2] <<std::endl;
-	// std::cout << numbers[3] <<std::endl;
-	// std::cout << numbers[4] <<std::endl;
-	// std::cout << numbers[5] <<std::endl;
-	// std::cout << numbers[6] <<std::endl;
-	// std::cout << numbers[7] <<std::endl;
-	// std::cout << numbers[8] <<std::endl;
-	// std::cout << numbers[9] <<std::endl;
-	// std::cout << numbers[10] <<std::endl;
+	std::cout << COLOUR_BLUE << "Testing the reverse_iterator. Checking if values are in reverse order" << COLOUR_RED << std::endl;
+	for(int i = 0; i < nums.size(); i++)
+		assert(nums.size() - i - 1 == *(r  + i));// 9 8 7 6 5 4 3 2 1 0
 }
