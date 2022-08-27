227aa1f656d98fd54e04ed47ff30ec708e0537ca
kmilchev <kmilchev@student.42wolfsburg.de>
2022-08-27 18:02:15 +0200

    testing with assert for vector_capacity

--- tests/test_vector_capacity.cpp
+++ tests/test_vector_capacity.cpp
@@ -11,85 +11,63 @@ int main(void)
 	const ft::vector<int>	my_v_const(3, 4);
 	const std::vector<int>	og_v_const(3, 4);
 
-	std::cout << COLOUR_BLUE << "Testing method .size()" << COLOUR_DEFAULT << std::endl;
-	std::cout << "Mine: should be 3: " << COLOUR_GREEN << my_v.size() << COLOUR_DEFAULT << std::endl;
-	std::cout << "OG: should be 3:   " << COLOUR_GREEN << og_v.size() << COLOUR_DEFAULT << std::endl;
+	std::cout << COLOUR_BLUE << "Testing method .size()" << COLOUR_RED << std::endl;
+	assert(3 == my_v.size());
 
-	std::cout << COLOUR_BLUE << "Testing method .max_size()" << COLOUR_DEFAULT << std::endl;
-	std::cout << "Mine: should be equal to OG: " << COLOUR_GREEN << my_v.max_size() << COLOUR_DEFAULT << std::endl;
-	std::cout << "OG:                          " << COLOUR_GREEN << og_v.max_size() << COLOUR_DEFAULT << std::endl;
+	std::cout << COLOUR_BLUE << "Testing method .max_size()" << COLOUR_RED << std::endl;
+	assert(og_v.max_size() == my_v.max_size());
 
-	std::cout << COLOUR_BLUE << "Testing method .capacity()" << COLOUR_DEFAULT << std::endl;
-	std::cout << "Mine: should be 3: " << COLOUR_GREEN << my_v.capacity() << COLOUR_DEFAULT << std::endl;
-	std::cout << "OG:   should be 3: " << COLOUR_GREEN << og_v.capacity() << COLOUR_DEFAULT << std::endl;
+	std::cout << COLOUR_BLUE << "Testing method .capacity()" << COLOUR_RED << std::endl;
+	assert(3 == my_v.capacity());
 
 	ft::vector<int> my_2nd_v;
-	std::cout << COLOUR_BLUE << "Testing method .empty()" << COLOUR_DEFAULT << std::endl;
-	std::cout << "Mine2: should be true: " << COLOUR_GREEN << std::boolalpha << my_2nd_v.empty() << COLOUR_DEFAULT << std::endl;
-	std::cout << "Mine: should be false: " << COLOUR_GREEN << std::boolalpha << my_v.empty() << COLOUR_DEFAULT << std::endl;
-	std::cout << "OG:   should be false: " << COLOUR_GREEN << std::boolalpha << og_v.empty() << COLOUR_DEFAULT << std::endl;
+	std::cout << COLOUR_BLUE << "Testing method .empty()" << COLOUR_RED << std::endl;
+	assert(true == my_2nd_v.empty());
+	assert(false == my_v.empty());
 
-	std::vector<int> og_new_v(1, 1);
-	ft::vector<int> my_new_v(1, 1);
-	std::cout << COLOUR_BLUE << "\nTesting method .resize() with sufficient capacity" << COLOUR_DEFAULT << std::endl;
-	og_new_v.resize(3);
-	my_new_v.resize(3);
-	std::cout << "OG:   Size should be 3: " << COLOUR_GREEN << og_new_v.size() << COLOUR_DEFAULT << std::endl;
-	std::cout << "Mine: Size should be 3: " << COLOUR_GREEN << my_new_v.size() << COLOUR_DEFAULT << std::endl;
+	{
+		std::cout << COLOUR_BLUE << "Testing method .resize() with sufficient capacity" << COLOUR_RED << std::endl;
+		std::vector<int> og_new_v(1, 1);
+		ft::vector<int> my_new_v(1, 1);
+		og_new_v.resize(3);
+		my_new_v.resize(3);
+		assert(3 == my_new_v.size());
+	}
 	
-	std::cout << COLOUR_BLUE << "\nTesting method .resize() with insufficient capacity" << COLOUR_DEFAULT << std::endl;
-	std::vector<int> og_newer_v;
-	ft::vector<int> my_newer_v;
-	og_newer_v.resize(0, 0);
-	my_newer_v.resize(0, 0);
-	std::cout << "OG:     Capacity should be 0: " << COLOUR_GREEN << og_newer_v.capacity() << COLOUR_DEFAULT << std::endl;
-	std::cout << "Mine:   Capacity should be 0: " << COLOUR_GREEN << my_newer_v.capacity() << COLOUR_DEFAULT << std::endl;
-	og_newer_v.resize(36, 2);
-	my_newer_v.resize(36, 2);
-	std::cout << "OG:     Resize(36, 0) Capacity should be 36: " << COLOUR_GREEN << og_newer_v.capacity() << COLOUR_DEFAULT << std::endl;
-	std::cout << "Mine:   Resize(36, 0) Capacity should be 36: " << COLOUR_GREEN << my_newer_v.capacity() << COLOUR_DEFAULT << std::endl;
-	std::cout << "Mine:   Resize(36, 0) Size     should be 36: " << COLOUR_GREEN << my_newer_v.size() << COLOUR_DEFAULT << std::endl;
-	og_newer_v.resize(77, 1);
-	my_newer_v.resize(77, 1);
-	std::cout << "OG:     Resize(77, 0) Capacity should be 77: " << COLOUR_GREEN << og_newer_v.capacity() << COLOUR_DEFAULT << std::endl;
-	std::cout << "Mine:   Resize(77, 0) Capacity should be 77: " << COLOUR_GREEN << my_newer_v.capacity() << COLOUR_DEFAULT << std::endl;
-	og_newer_v.resize(78, 1);
-	my_newer_v.resize(78, 1);
-	std::cout << "OG:     Resize(78, 0) Capacity should be 154: " << COLOUR_GREEN << og_newer_v.capacity() << COLOUR_DEFAULT << std::endl;
-	std::cout << "Mine:   Resize(78, 0) Capacity should be 154: " << COLOUR_GREEN << my_newer_v.capacity() << COLOUR_DEFAULT << std::endl;
-	for (int i = 0; i < og_newer_v.size(); i++)
 	{
-		std::cout << COLOUR_YELLOW << og_newer_v[i] << " ";
-	}
-	std::cout<< std::endl;
-	for (int i = 0; i < my_newer_v.size(); i++)
-	{
-		std::cout << COLOUR_GREEN << my_newer_v[i] << " ";
-	}
-	std::vector<int> og_vec(2, 2);
-	ft::vector<int> my_vec(2, 2);
-	std::cout << COLOUR_DEFAULT << std::endl;
-	std::cout << COLOUR_BLUE << "\nTesting method .reserve()" << COLOUR_DEFAULT << std::endl;
-	std::cout << "OG:   Capacity should be 2: " << COLOUR_GREEN << og_vec.capacity() << COLOUR_DEFAULT << std::endl;
-	std::cout << "Mine: Capacity should be 2: " << COLOUR_GREEN << my_vec.capacity() << COLOUR_DEFAULT << std::endl;
-	og_vec.reserve(70);
-	my_vec.reserve(70);
-	std::cout << "OG:   .reserve(70)Capacity should be 70: " << COLOUR_GREEN << og_vec.capacity() << COLOUR_DEFAULT << std::endl;
-	std::cout << "Mine: .reserve(70)Capacity should be 70: " << COLOUR_GREEN << my_vec.capacity() << COLOUR_DEFAULT << std::endl;
-	og_vec.reserve(1);
-	my_vec.reserve(1);
-	std::cout << "OG:   .reserve(1)Capacity should be 70: " << COLOUR_GREEN << og_vec.capacity() << COLOUR_DEFAULT << std::endl;
-	std::cout << "Mine: .reserve(1)Capacity should be 70: " << COLOUR_GREEN << my_vec.capacity() << COLOUR_DEFAULT << std::endl;
-	for (int i = 0; i < og_vec.size(); i++)
-	{
-		std::cout << COLOUR_YELLOW << og_vec[i] << " ";
-	}
-	std::cout<< std::endl;
-	for (int i = 0; i < my_vec.size(); i++)
-	{
-		std::cout << COLOUR_GREEN << my_vec[i] << " ";
+		std::cout << COLOUR_BLUE << "Testing method .resize() with insufficient capacity" << COLOUR_RED << std::endl;
+		std::vector<int> og_newer_v;
+		ft::vector<int> my_newer_v;
+		og_newer_v.resize(0, 0);
+		my_newer_v.resize(0, 0);
+		assert(og_newer_v.capacity() == my_newer_v.capacity());
+		og_newer_v.resize(36, 2);
+		my_newer_v.resize(36, 2);
+		assert(og_newer_v.capacity() == my_newer_v.capacity());
+		assert(og_newer_v.size() == my_newer_v.size());
+		og_newer_v.resize(77, 1);
+		my_newer_v.resize(77, 1);
+		assert(og_newer_v.capacity() == my_newer_v.capacity());
+		assert(og_newer_v.size() == my_newer_v.size());
+		og_newer_v.resize(78, 1);
+		my_newer_v.resize(78, 1);
+		assert(og_newer_v.capacity() == my_newer_v.capacity());
+		assert(og_newer_v.size() == my_newer_v.size());
+		for (int i = 0; i < og_newer_v.size(); i++)
+			assert(og_newer_v[i] == my_newer_v[i]);
 	}
 
-	std::cout<< std::endl;
-
+	{
+		std::cout << COLOUR_BLUE << "Testing method .reserve()" << COLOUR_RED << std::endl;
+		std::vector<int> og_vec(2, 2);
+		ft::vector<int> my_vec(2, 2);
+		og_vec.reserve(70);
+		my_vec.reserve(70);
+		assert(og_vec.capacity() == my_vec.capacity());
+		og_vec.reserve(1);
+		my_vec.reserve(1);
+		assert(og_vec.capacity() == my_vec.capacity());
+		for (int i = 0; i < og_vec.size(); i++)
+			assert(og_vec[i] == my_vec[i]);
+	}
 }
