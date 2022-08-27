641dfcf5aad9733c730bd88e4f15c68e5b00c966
kmilchev <kmilchev@student.42wolfsburg.de>
2022-08-27 20:35:02 +0200

    assert tests for vector_non_member_swap

--- tests/test_vector_non_member_swap.cpp
+++ tests/test_vector_non_member_swap.cpp
@@ -6,7 +6,7 @@
 /*   By: kmilchev <kmilchev@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 21:08:48 by kmilchev          #+#    #+#             */
-/*   Updated: 2022/08/26 15:43:22 by kmilchev         ###   ########.fr       */
+/*   Updated: 2022/08/27 20:34:42 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,24 @@
 #include <vector>
 #include <iterator>
 #include "colours.h"
+#include "utils.hpp"
 
 int main(void)
 {
 	ft::vector<int> v1(2, 2);
 	ft::vector<int> v2(3, 3);
+	ft::vector<int> v1_copy(v1);
+	ft::vector<int> v2_copy(v2);
 
-	std::cout << COLOUR_PURPLE << "Before swap: " << std::endl;
-	for (int i = 0; i < v1.size(); i++)
-		std::cout << COLOUR_YELLOW << v1[i] << " ";
-	std::cout << COLOUR_DEFAULT << std::endl;
-	
-	for (int i = 0; i < v2.size(); i++)
-		std::cout << COLOUR_GREEN << v2[i] << " ";
-	std::cout << COLOUR_DEFAULT << std::endl;
-	
-	ft::swap(v1, v2);
-	std::cout << COLOUR_PURPLE << "After swap: " << std::endl;
-	for (int i = 0; i < v1.size(); i++)
-		std::cout << COLOUR_GREEN << v1[i] << " ";
-	std::cout << COLOUR_DEFAULT << std::endl;
-	
-	for (int i = 0; i < v2.size(); i++)
-		std::cout << COLOUR_YELLOW << v2[i] << " ";
-	std::cout << COLOUR_DEFAULT << std::endl;
+	{
+		test_name("Test swap");
+		//befor swap
+		for (int i = 0; i < v1.size(); i++)
+			assert(v1_copy[i] == v1[i]);
+			
+		ft::swap(v1, v2);
+		//after swap
+		for (int i = 0; i < v1.size(); i++)
+			assert(v1_copy[i] == v2[i]);
+	}
 }
