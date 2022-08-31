6fb8cc2ca6da773992512612afd39d855c135f39
kmilchev <kmilchev@student.42wolfsburg.de>
2022-08-31 23:08:42 +0200

    starting to explore map. most typedefs (no iterator) and the nested class value_compare done

--- ft_map.hpp
+++ ft_map.hpp
@@ -6,7 +6,47 @@
 /*   By: kmilchev <kmilchev@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 23:01:26 by kmilchev          #+#    #+#             */
-/*   Updated: 2022/08/28 23:01:27 by kmilchev         ###   ########.fr       */
+/*   Updated: 2022/08/31 23:07:11 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <functional>
+#include <utility>
+#include <memory>
+
+namespace ft
+{
+	template <	class Key, class T, class Compare = std::less<Key>,
+				class Allocator = std::allocator<std::pair<const Key, T> > >
+	class map
+	{
+		public:
+			typedef Key												key_type;
+			typedef T												mapped_type;
+			typedef std::pair<const key_type, mapped_type>			value_type; //need to implement pair myself //so no std..
+			typedef Compare											key_compare;
+			typedef Allocator										allocator_type;
+			typedef typename allocator_type::reference				reference;
+			typedef typename allocator_type::const_reference		const_reference;
+			typedef typename allocator_type::pointer				pointer;
+			typedef typename allocator_type::const_pointer			const_pointer;
+			typedef typename allocator_type::size_type				size_type;
+			typedef typename allocator_type::difference_type		difference_type;
+
+		
+		class value_compare : public std::binary_function<value_type, value_type, bool>
+		{
+			friend class map;
+
+			protected:
+				key_compare comp;
+				value_compare(key_compare c) : comp(c) {}
+			public:
+				bool operator()(const value_type &lhs, const value_type &rhs) const
+					{return comp(lhs.first, rhs.second);}
+		};
+		
+		explicit map (	const key_compare &comp = key_compare(),
+						const allocator_type &alloc = allocator_type()); 
+	};
+};
--- main.cpp
+++ main.cpp
@@ -6,26 +6,84 @@
 /*   By: kmilchev <kmilchev@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 15:51:53 by kmilchev          #+#    #+#             */
-/*   Updated: 2022/08/22 12:40:04 by kmilchev         ###   ########.fr       */
+/*   Updated: 2022/08/31 22:41:39 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-	// comparing size, capacity and max_size
+// #include <iostream>
+#include <map>
+// #include <stack>
+
+// #include <iostream>
+// #include <string>
+// #include <deque>
+// #if 1 //CREATE A REAL STL EXAMPLE
+// 	#include <map>
+// 	#include <stack>
+// 	#include <vector>
+// 	namespace ft = std;
+// #else
+// 	#include <ft_map.hpp>
+// 	#include <ft_stack.hpp>
+// 	#include <ft_vector.hpp>
+// #endif
+
+#include <stdlib.h>
+class Node
+{
+	public:
+		int i;	
+		Node *left;
+		Node *right;
+	
+	Node(int val) : i(val), left(NULL), right(NULL) {};
+};
+
+#include "ft_map.hpp"
+// int main(void)
+// {
+// 	Node *root = new Node(1);
+	
+// 	root->left = new Node(2);
+// 	root->left = new Node(3);
+	
+// 	root->left->left = new Node(4);
+	
+
+	
+// 	return (0);
+// }
+
+
 #include <iostream>
-#include <vector>
+#include <map>
+#include <functional>
+
+template <class T, class U>
+class Test_Functor : public std::binary_function<T, U, bool>
+{
+	// int i = int();
+	
+	public:
+		typedef	std::binary_function<T, U, bool> 						templ;
+		typedef typename templ::first_argument_type						word;
+		typedef typename templ::second_argument_type					word2;
+		void operator()(void) {std::cout << "Whatevs\n";}
+		
+		word do_nothing(){return (27.8);};
+		word2 do_nothing_still(){return (43.3);};
+
+		
+		
+};
 
 int main ()
 {
-  std::vector<int> myvector;
-
-  // set some content in the vector:
-  // for (int i=0; i<100; i++) myvector.push_back(i);
-
-  // std::cout << "size: " << myvector.size() << "\n";
-  // std::cout << "capacity: " << myvector.capacity() << "\n";
-  // std::cout << "max_size: " << myvector.max_size() << "\n";
- 
-  int *data = myvector.data();
-  std::cout << "First element: " << (data) << std::endl;
-  return 0;
+	// std::binary_function<int, int, bool> confustion;
+	Test_Functor<int, float> test;
+	// test.first_argument_type;
+	std::cout << test.do_nothing();
+	std::cout << test.do_nothing_still();
+	
+	return 0;
 }
