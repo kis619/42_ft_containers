9bf88931960d7220185e7f0d628fe5e96237512e
kmilchev <kmilchev@student.42wolfsburg.de>
2022-09-04 22:23:34 +0200

    updated header defs

--- ft_rbt_iterator.hpp
+++ ft_rbt_iterator.hpp
@@ -1,19 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.hpp                                          :+:      :+:    :+:   */
+/*   ft_rbt_iterator.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kmilchev <kmilchev@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/27 18:32:14 by kmilchev          #+#    #+#             */
-/*   Updated: 2022/09/04 19:57:15 by kmilchev         ###   ########.fr       */
+/*   Created: 2022/09/04 19:26:13 by kmilchev          #+#    #+#             */
+/*   Updated: 2022/09/04 19:27:31 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef UTILS
-# define UTILS
-# include <iostream>
-
-void	test_name(std::string name);
-
-#endif //UTILS_HPP
+#include "ft_iterator_traits.hpp"
