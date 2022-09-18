2102b21a62ce98c858c945685674adfcb56f3464
kmilchev <kmilchev@student.42wolfsburg.de>
2022-09-18 18:15:31 +0200

    made blue bold

--- tests/colours.h
+++ tests/colours.h
@@ -6,7 +6,7 @@
 /*   By: kmilchev <kmilchev@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 12:03:18 by kmilchev          #+#    #+#             */
-/*   Updated: 2022/08/26 15:31:37 by kmilchev         ###   ########.fr       */
+/*   Updated: 2022/09/18 18:15:14 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # define COLOUR_GREEN "\33[32m"
 # define COLOUR_RED "\33[1;31m"
 # define COLOUR_YELLOW "\33[33m"
-# define COLOUR_BLUE "\33[34m"
+# define COLOUR_BLUE "\33[1;34m"
 # define COLOUR_PURPLE "\33[35m"
 
 #endif
