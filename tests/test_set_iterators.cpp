1ce7349b6dcec6baebb5c86b6fba2293cecb0205
kmilchev <kmilchev@student.42wolfsburg.de>
2022-10-01 11:33:43 +0200

    iterator tests

--- tests/test_set_iterators.cpp
+++ tests/test_set_iterators.cpp
@@ -6,7 +6,7 @@
 /*   By: kmilchev <kmilchev@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 09:56:37 by kmilchev          #+#    #+#             */
-/*   Updated: 2022/09/30 16:15:37 by kmilchev         ###   ########.fr       */
+/*   Updated: 2022/10/01 11:33:17 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,15 +28,48 @@ int main(void)
 	ft::set<int> my_set;
 	std::set<int> og_set;
 	
-	og_set.insert(og_set.begin(), 6);
-	std::set<int>::iterator it = og_set.find(234);
-	std::cout << it.operator->() << std::endl;
-	std::cout << *(it) << std::endl;
+	for (int i = 0; i < 10; i++)
+	{
+		og_set.insert(i + 1);
+		my_set.insert(i + 1);
+	}
 	
-	my_set.insert(0, 19);
-	// ft::set<int>::iterator my_it = my_set.find(6);
-	std::cout << my_set.count(19);
-	std::cout << my_set.count(33);
+	{
+		test_name("Testing the method .begin()");
+		std::set<int>::iterator og_it = og_set.begin();
+		ft::set<int>::iterator my_it = my_set.begin();
+		for (int i = 0; i < 10; i++)
+			assert(*(my_it++) == *(og_it++));
+	}
+	
+	{
+		test_name("Testing the method .end()");
+		std::set<int>::iterator og_it = og_set.end();
+		ft::set<int>::iterator my_it = my_set.end();
+		og_it--;
+		my_it--;
+		for (int i = 0; i < 10; i++)
+			assert(*(my_it--) == *(og_it--));
+	}
+	
+	{
+		test_name("Testing the method .rend()");
+		std::set<int>::reverse_iterator og_rev_it = og_set.rend();
+		ft::set<int>::reverse_iterator my_rev_it = my_set.rend();
+		og_rev_it--;
+		my_rev_it--;
+		for (int i = 0; i < 9; i++)
+			assert(*(og_rev_it--) == *(my_rev_it--));
+	}
+	
+	{
+		test_name("Testing the method .rbegin()");
+		std::set<int>::reverse_iterator og_rev_it = og_set.rbegin();
+		ft::set<int>::reverse_iterator my_rev_it = my_set.rbegin();
+		for (int i = 0; i < 10; i++)
+			assert(*(og_rev_it++) == *(my_rev_it++));
+	}
+	
 	std::cout << COLOUR_DEFAULT;
 	return (0);
 }
